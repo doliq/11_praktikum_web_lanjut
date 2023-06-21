73d6aa7163dd162221fb9c89ea3f95fb0e1732ed
doliq <diwaoliq@gmail.com>
2023-06-21 13:48:33 +0700

    done

---
+++ app/Http/Controllers/Api/TodoController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Todo;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\TodoRequest;
+use App\Http\Controllers\Controller;
+
+class TodoController extends Controller
+{
+    use ApiResponse;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = auth()->user();
+        $todos = Todo::with('user')->where('user_id', $user->id)->get();
+
+        return $this->apiSuccess($todos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TodoRequest $request)
+    {
+        $request->validated();
+
+        $user = auth()->user();
+        $todo = new Todo($request->all());
+        $todo->user()->associate($user);
+        $todo->save();
+
+        return $this->apiSuccess($todo->load('user'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        return $this->apiSuccess($todo->load('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(TodoRequest $request, Todo $todo)
+    {
+        $request->validated();
+        $todo->todo = $request->todo;
+        $todo->label = $request->label;
+        $todo->done = $request->done;
+        $todo->save();
+
+        return $this->apiSuccess($todo->load('user'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->delete();
+            return $this->apiSuccess($todo);
+        }
+        return $this->apiError(
+            'Unauthorized',
+            Response::HTTP_UNAUTHORIZED,
+        );
+    }
+}
---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\ApiResponse;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    }
+}
